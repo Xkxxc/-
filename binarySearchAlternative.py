fd54ae4282c213a455d73074c85658a00e98682e
Xkxxc <100979479+Xkxxc@users.noreply.github.com>
2023-04-24 20:09:42 +0800

    Add files via upload

---
+++ binarySearchAlternative.py
@@ -0,0 +1,14 @@
+def binarysearch(list,target):
+    i = 0
+    j = len(list)
+    while i < j:    #不要写<=,如果要查找的值不在列表中,则会造成死循环!
+        m = (i + j) >> 1
+        if target < list[m]:
+            j = m
+        elif list[m] < target:
+            i = m + 1
+        else:
+            return m
+    return -1
+
+
